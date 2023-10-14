2016d234b5e463307b7add2fd453f058d86c1c12
Sempai-07 <suhozadvova7@gmail.com>
2023-10-14 08:06:40 +0300

    test webhook

--- src/function/message/message.ts
+++ src/function/message/message.ts
@@ -5,5 +5,5 @@ export const data = {
     const argsFunc = event.text?.split(/\s+/).slice(1)?.[Number(args[0]) - 1];
     const noArgsFunc = event.text?.split(/\s+/).slice(1).join(" ");
     return args[0] === undefined ? noArgsFunc : argsFunc;
-  }
+  },
 };
