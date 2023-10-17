b3bcce6c7b939ad658e121944fec11bf4e82c107
Sempai-07 <suhozadvova7@gmail.com>
2023-10-17 20:48:01 +0300

    added languageCode

---
+++ src/function/info/languageCode.ts
@@ -0,0 +1,11 @@
+import { DataFunction } from "context";
+
+const data: DataFunction = {
+  name: "$languageCode",
+  callback: async (ctx, event, database, error) => {
+    const languageCode = event.from.language_code ?? event.message?.from.language_code;
+    return languageCode || null;
+  },
+};
+
+export { data };
