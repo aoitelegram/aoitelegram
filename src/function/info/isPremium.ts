35409bf86e255857d8a397b0bce547e156aa1eb5
Sempai-07 <suhozadvova7@gmail.com>
2023-10-17 20:47:29 +0300

    added isPremium

---
+++ src/function/info/isPremium.ts
@@ -0,0 +1,11 @@
+import { DataFunction } from "context";
+
+const data: DataFunction = {
+  name: "$isPremium",
+  callback: async (ctx, event, database, error) => {
+    const isPremium = event.from.is_premium ?? event.message?.from.is_premium;
+    return isPremium || null;
+  },
+};
+
+export { data };
