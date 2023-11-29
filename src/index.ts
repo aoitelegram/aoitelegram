fff19734087cb4915d7cf62a2e10a2ebb3c8b8ce
Sempai-07 <mausm471@gmail.com>
2023-11-29 15:54:53 +0200

    new export class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import { AoiClient } from "./classes/AoiClient";
+import { AoijsError } from "./classes/AoiError";
+import { AoiManager } from "./classes/AoiManager";
+import { AoiWarning } from "./classes/AoiWarning";
 import { PluginManager } from "./classes/PluginManager";
 import { LoadCommands } from "./classes/LoadCommands";
-import { DataFunction } from "./classes/AoiBase";
+import { AoiBase, DataFunction } from "./classes/AoiBase";
 import { version } from "../package.json";
 
-export { AoiClient, PluginManager, LoadCommands, DataFunction, version };
+export {
+  AoiClient,
+  AoiBase,
+  AoiWarning,
+  AoiManager,
+  AoijsError,
+  PluginManager,
+  LoadCommands,
+  DataFunction,
+  version,
+};
 export default {
   AoiClient,
+  AoiBase,
+  AoiWarning,
+  AoiManager,
+  AoijsError,
   PluginManager,
   LoadCommands,
   version,
