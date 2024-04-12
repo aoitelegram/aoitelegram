8123452cb957ce4b157d78ce4321cd942d5337a3
Sempai-07 <mausm471@gmail.com>
2024-04-12 17:02:37 +0300

    added polyfill Array.at

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
+Array.prototype.at ??= function (index: number) {
+  return index >= 1 ? this[index] : this[this.length + index];
+};
+
 export * from "./AoiStart";
 export * from "./Condition";
 export * from "./Parser";
