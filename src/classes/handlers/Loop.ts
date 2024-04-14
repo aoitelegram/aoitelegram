5ebedd3141d774e9ea14d6ef22db0b6691662486
Sempai-07 <mausm471@gmail.com>
2024-04-14 11:43:08 +0300

    added handlers message_reaction_count and loop

--- src/classes/handlers/Loop.ts
+++ src/classes/handlers/Loop.ts
@@ -1,5 +1,37 @@
 import type { AoiClient } from "../AoiClient";
+import { setInterval, clearInterval } from "long-timeout";
 
-function onLoop(telegram: AoiClient) {}
+function onLoop(telegram: AoiClient): void {
+  const events = telegram.events.get("loop");
+  if (!events) return;
+
+  for (const event of events) {
+    let currentIndex = 1;
+    const intervalId = setInterval(async () => {
+      telegram.ensureCustomFunction([
+        {
+          name: "$break",
+          brackets: false,
+          callback: (context, func) => {
+            clearInterval(intervalId);
+            return func.resolve();
+          },
+        },
+        {
+          name: "$continue",
+          brackets: false,
+          callback: (context, func) => func.reject(),
+        },
+        {
+          name: "$index",
+          callback: (context, func) => func.resolve(currentIndex),
+        },
+      ]);
+
+      await telegram.evaluateCommand(event, telegram);
+      currentIndex++;
+    }, event.every || 60000);
+  }
+}
 
 export default onLoop;
--- src/classes/handlers/MessageReactionCount.ts
+++ src/classes/handlers/MessageReactionCount.ts
@@ -1,5 +1,14 @@
 import type { AoiClient } from "../AoiClient";
 
-function onMessageReactionCount(telegram: AoiClient) {}
+function onMessageReactionCount(telegram: AoiClient): void {
+  const events = telegram.events.get("messageReactionCount");
+  if (!events) return;
+
+  for (const event of events) {
+    telegram.on("message_reaction_count", async (ctx) => {
+      await telegram.evaluateCommand(event, ctx);
+    });
+  }
+}
 
 export default onMessageReactionCount;
